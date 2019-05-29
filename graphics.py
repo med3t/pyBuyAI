99a616ed009cbf58eacbf5bc8f2151c0721e5053
Mr. M <medetkanmaz@gmail.com>
2019-05-29 18:59:02 +0300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -99,6 +99,28 @@ def plot_rewards_per_episode(df,alpha=(0.7,1),colours=('tab:blue','tab:purple'),
     plt.plot(df2['episode'], df2['reward'], alpha=alpha[0], color='tab:blue', label='Reward')
     plt.plot(df2['episode'], df2['reward_ma'], alpha=alpha[1], color='tab:purple', label='{} game moving avg'.format(rolling_mean_window))
     plt.legend(loc='lower right')
+    axs.set_xlabel('Bid Episode')
+    axs.set_ylabel('Reward')
+    return (fig,axs)
+
+def plot_game_ending_periods(df,alpha=(0.7,1),colours=('tab:blue')):
+    """
+    Line plot of rewards per episode
+    :param df:
+    :param alpha:
+    :param colours:
+    :return:
+    """
+    
+    df1 = df[["episode","bidding_round","is_game_ending"]]
+    df1 = df1[df1["bidding_round"]>0]
+
+    df2 = df1[df1["is_game_ending"]==True].groupby("episode")["bidding_round"].min().reset_index(name ='period_converged')
+    fig, axs = plt.subplots(1)
+    plt.plot(df2['episode'], df2['period_converged'], alpha=alpha[0], color='tab:blue', label='Converged Period ')
+    plt.legend(loc='lower right')
+    axs.set_xlabel('Bid Episode')
+    axs.set_ylabel('Bid Period')
     return (fig,axs)
 
 if __name__ == '__main__':
@@ -108,7 +130,7 @@ if __name__ == '__main__':
     #get multiagent final bid heatmaps
     player0 = 'dd6daa2f-cc2c-40db-a42f-6e4ab6710f81'
     player1 = '8b68992c-c44b-475d-b3ee-c67a9b3b8ab6'
-    folder = r"C:\GitRepo\pyBuyAI\parameter_grid_search\results\alpha_gamma_gridsearch_10k_2bp"
+    folder = r"/Users/med/Desktop/MMI/MMI706/pyBuyAI-master/parameter_grid_search/results/alpha_gamma_gridsearch_10k_2bp"
 
     player0_serialised = os.path.join(folder,'player_0_'+player0+'.npy')
     #player0_path = os.path.join(folder,'player_0_'+game_id+'.hdf')
@@ -122,7 +144,7 @@ if __name__ == '__main__':
     player1.load_serialised_agent(player1_serialised)
 
     #get results paths graphics for a good agent
-    results_folder = r'C:\GitRepo\pyBuyAI\parameter_grid_search2\results\epsilon_decay_gridsearch_10kg_4bp'
+    results_folder = r"/Users/med/Desktop/MMI/MMI706/pyBuyAI-master/parameter_grid_search2/results/epsilon_decay_gridsearch_10kg_4bp"
     game_id = '533e37b7-983c-4e45-93c7-7cb02d67048d'
     path_hdf = os.path.join(results_folder,'player_0_'+game_id+'.hdf')
     path_df = pd.read_csv(path_hdf,sep='#')
@@ -136,7 +158,7 @@ if __name__ == '__main__':
 
 
     # get grid search heatmaps
-    results_template = r"C:\GitRepo\pyBuyAI\parameter_grid_search2\results\*results*csv"
+    #results_template = "/Users/med/Desktop/MMI/MMI706/pyBuyAI-master/parameter_grid_search2/results/*results*csv"
     results_files = glob.glob(results_template)
 
     df = pd.read_csv(results_files[0])
@@ -168,12 +190,6 @@ if __name__ == '__main__':
         axs[i].xaxis.tick_top()
         axs[i].set_title(metric_titles[i])
         sns.heatmap(piv, cmap=cmap_list[i], ax=axs[i], linewidths=1)
-
-    #plt.show()
-
-    # for alpha and gamma
-    results_template = r"C:\GitRepo\pyBuyAI\parameter_grid_search\results\*results*csv"
-    results_files = glob.glob(results_template)
 
     df = pd.read_csv(results_files[0])
     for file in results_files[1:]:
@@ -222,18 +238,13 @@ if __name__ == '__main__':
             aggfunc='mean',
             fill_value=0
         )
-        #fig, axs = plt.subplots(1)
-        #fig.suptitle('Grid search on: {}'.format(', '.join(grid_params)))
         axs[i].xaxis.tick_top()
         axs[i].set_title(metric_titles[i])
         sns.heatmap(piv, cmap=cmap_list[i], ax=axs[i], linewidths=1)
 
-        #plt.show()
-
     #get multiagent final bid heatmaps
     game_id = '67835b1b-3e53-4263-b4c6-479d06894375'
-    folder = r"C:\GitRepo\pyBuyAI\parameter_grid_search\results"
-
+    folder = "/Users/med/Desktop/MMI/MMI706/pyBuyAI-master/parameter_grid_search/results"
     player0_serialised = os.path.join(folder,'player_0_'+game_id+'.npy')
     #player0_path = os.path.join(folder,'player_0_'+game_id+'.hdf')
     player1_serialised = os.path.join(folder,'player_1_'+game_id+'.npy')
@@ -262,76 +273,3 @@ if __name__ == '__main__':
 
     #look at qmatrix and r matrix
     State = env.define_state()
-    S = [State(current_winner=nan, current_bids=(nan, nan)),
-         State(current_winner=1, current_bids=(nan, 0)),
-         State(current_winner=1, current_bids=(nan, 1)),
-         State(current_winner=1, current_bids=(nan, 2)),
-         State(current_winner=1, current_bids=(nan, 3)),
-         State(current_winner=1, current_bids=(nan, 4)),
-         State(current_winner=0, current_bids=(0, nan)),
-         State(current_winner=nan, current_bids=(0, 0)),
-         State(current_winner=1, current_bids=(0, 1)),
-         State(current_winner=1, current_bids=(0, 2)),
-         State(current_winner=1, current_bids=(0, 3)),
-         State(current_winner=1, current_bids=(0, 4)),
-         State(current_winner=0, current_bids=(1, nan)),
-         State(current_winner=0, current_bids=(1, 0)),
-         State(current_winner=nan, current_bids=(1, 1)),
-         State(current_winner=1, current_bids=(1, 2)),
-         State(current_winner=1, current_bids=(1, 3)),
-         State(current_winner=1, current_bids=(1, 4)),
-         State(current_winner=0, current_bids=(2, nan)),
-         State(current_winner=0, current_bids=(2, 0)),
-         State(current_winner=0, current_bids=(2, 1)),
-         State(current_winner=nan, current_bids=(2, 2)),
-         State(current_winner=1, current_bids=(2, 3)),
-         State(current_winner=1, current_bids=(2, 4)),
-         State(current_winner=0, current_bids=(3, nan)),
-         State(current_winner=0, current_bids=(3, 0)),
-         State(current_winner=0, current_bids=(3, 1)),
-         State(current_winner=0, current_bids=(3, 2)),
-         State(current_winner=nan, current_bids=(3, 3)),
-         State(current_winner=1, current_bids=(3, 4)),
-         State(current_winner=0, current_bids=(4, nan)),
-         State(current_winner=0, current_bids=(4, 0)),
-         State(current_winner=0, current_bids=(4, 1)),
-         State(current_winner=0, current_bids=(4, 2)),
-         State(current_winner=0, current_bids=(4, 3)),
-         State(current_winner=nan, current_bids=(4, 4))]
-    cb = [s.current_bids for s in S]
-    pd.options.display.max_columns = 40
-    thing = pd.DataFrame(data=player1.Q[0], columns=cb, index=cb)
-    thing
-
-    print('done')
-    """
-    ept = 0.6
-    df2 = df[(df['bid_periods'] == 5) & (df['epsilon_threshold'] == ept)]
-
-    fig,axs=plot_grid_search_heatmap('epsilon_decay_1','epsilon_decay_2','Period Converged',df2)
-    fig.suptitle('Epsilon Threshold: ept')
-
-    fig, axs = plt.subplots(1, 3, figsize=(15, 15), sharex=True, sharey=True,
-                            tight_layout=True)
-    cbar_ax = fig.add_axes([.91, .3, .03, .4])
-
-    for i,th in enumerate([0.2,0.4,0.6]):
-        df2 = df[(df['bid_periods'] == 5) & (df['epsilon_threshold'] == th)]
-        df3 = df2.pivot('epsilon_decay_1','epsilon_decay_2','Period Converged')
-        im = sns.heatmap(df3,ax=axs[i],cbar=i == 0,cbar_ax=None if i else cbar_ax)
-        axs[i].set_title('Epsilon threshold: {0}'.format(th))
-
-    fig.suptitle('Episodes until convergence for epsilon decay configurations')
-
-    fig.subplots_adjust(right=0.8)
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.colorbar(im, cax=cbar_ax)
-
-    fig = plt.figure()
-    plt.plot(df2['episode'], df2['reward'])
-    plt.show()
-    """
-
-
-
-    print('Done generating graphics')
